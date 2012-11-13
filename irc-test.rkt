5ca2ec42d4cc63bd052de0150b2de9fda9653293
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2012-11-12 22:13:53 -0500

    Got a GUI client working, starting to make a real API

--- irc-test.rkt
+++ irc-test.rkt
@@ -1,26 +1,55 @@
 #lang racket
 
 (require "irc.rkt")
+(require racket/gui/base)
 
-(define connection (irc-get-connection "chat.freenode.net" 6667))
-(define incoming (irc-connection-in-port connection))
+(define window-frame (new frame% [label "IRC Test Client"]))
+(define output-area (new editor-canvas% [parent window-frame]
+			                [editor (new text%)]
+					[min-width 800]
+					[min-height 400]))
+
+(define button (new button%
+		    [parent window-frame]
+		    [label "Send"]
+		    [callback
+		     (lambda (b e)
+		       (irc-send-message
+			connection
+			"##racketirctest"
+			(send input-field get-value))
+		       (send input-field set-value ""))]))
+
+(define input-field (new text-field%
+			 [label "Message"]
+			 [parent window-frame]
+			 [min-width 200]))
+			 
+
+(define (add-text t)
+  (send (send output-area get-editor) insert t))
+
+(send window-frame show #t)
+
+(define connection (irc-connect "chat.freenode.net" 6667 "rackirc" "Racket Test Client"))
 
 (define reader-thread
   (thread (lambda ()
+	    (define incoming (irc-connection-in-port connection))
+	    (irc-join-channel connection "##racketirctest")
+	    (irc-send-message connection "##racketirctest" "Hello")
+
           (let loop ()
             (define line (read-line incoming))
             (unless (eof-object? line)
+	      (define raw-message (read-line incoming))
+	      (add-text (string-append "Message: " raw-message "\n"))
               (display "Message: ")
-              (display (read-line incoming))
+              (display raw-message)
               (newline)
               (loop))))))
 
-(irc-send-command connection "NICK" "rackirc")
-(irc-send-command connection "USER" "rackirc" "0" "*" ":TesterSchu")
-(irc-send-command connection "JOIN" "##racketirctest")
-(irc-send-command connection "PRIVMSG" "##racketirctest" ":Hello, world")
-
-(thread-wait reader-thread)
+;;(thread-wait reader-thread)
 
 ; TODO: implement message send as the following, over some given connection
 ; (irc-send-message command parameter ... #:prefix prefix)
--- irc.rkt
+++ irc.rkt
@@ -1,8 +1,55 @@
 #lang racket/base
+
+(require racket/string)
 (require racket/tcp)
 
-; send NICK jschuster_
 
-(define-values (outgoing incoming) (tcp-connect "chat.freenode.net" 6667))
+(provide irc-get-connection
+         irc-connection-in-port ; TODO: remove this
+         irc-send-command
+	 irc-send-message
+	 irc-join-channel
+	 irc-connect
+	 irc-set-nick
+	 irc-set-user-info)
 
-; "NICK jschuster_\r\n"
+(struct irc-connection (in-port out-port))
+
+(define (irc-get-connection host port)
+  (let-values ([(in out) (tcp-connect host port)])
+    (file-stream-buffer-mode out 'line)    
+    (irc-connection in out)))
+
+(define (irc-send-command connection command . parameters)
+  (fprintf (irc-connection-out-port connection)
+           "~a ~a\r\n"
+           command
+           (string-join parameters)))
+
+(define (irc-set-nick connection nick)
+  (irc-send-command connection "NICK" "rackirc"))
+
+(define (irc-set-user-info connection nick real-name)
+  (irc-send-command connection
+		    "USER"
+		    nick
+		    "0"
+		    "*"
+		    (string-append ":" real-name)))
+
+(define (irc-connect server port nick real-name)
+  (define-values (in out) (tcp-connect server port))
+  (file-stream-buffer-mode out 'line)    
+  (define connection (irc-connection in out))
+  (irc-set-nick connection nick)
+  (irc-set-user-info connection nick real-name)
+  connection)
+
+(define (irc-join-channel connection channel)
+  (irc-send-command connection "JOIN" channel))
+
+(define (irc-send-message connection target message)
+  (irc-send-command connection
+		    "PRIVMSG"
+		    target
+		    (string-append ":" message)))
