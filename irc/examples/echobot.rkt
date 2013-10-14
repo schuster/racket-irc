cf5f98a07834d32e740ab86c9ed855d9ac4a7feb
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2013-10-13 22:57:51 -0400

    Fixed echobot to work with current version of library

--- irc/examples/echobot.rkt
+++ irc/examples/echobot.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
 (require racket/async-channel)
-(require "../main.rkt")
+(require irc)
 
-(define connection (irc-connect "chat.freenode.net" 6667 "schubot" "Schuster's Echo Bot"))
-(sleep 5)
+(define-values (connection ready-event)
+  (irc-connect "chat.freenode.net" 6667 "schubot" "schubot" "Schuster's Echo Bot"))
+(void (sync ready-event))
 
 (irc-join-channel connection "##racketirctest")
 (define incoming (irc-connection-incoming connection))
@@ -12,8 +13,7 @@
 (let loop ()
   (define message (async-channel-get incoming))
   (match message
-    [(irc-message _ #f _ _) (void)]
-    [(irc-message _ prefix "PRIVMSG" params)
+    [(irc-message prefix "PRIVMSG" params _)
      (define prefix-match (regexp-match #rx"^[^!]+" prefix))
      (define message-match (regexp-match #rx"schubot: (.*)" (second params)))
      (when (and prefix-match message-match)
