4be4676f229dc02d83bffe10280ad4874ee62e48
Leif Andersen <leif@leifandersen.net>
2015-02-01 09:34:30 -0500

    Add racket-doc to build-deps

--- info.rkt
+++ info.rkt
@@ -2,5 +2,5 @@
 
 (define version "1.0")
 (define deps (list "base"))
-(define build-deps (list "rackunit-lib" "scribble-lib"))
+(define build-deps (list "rackunit-lib" "scribble-lib" "racket-doc"))
 (define collection 'multi)
